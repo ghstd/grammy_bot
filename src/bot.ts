d204cb76575181f4400bc3955c0fc17fb5a8db58
AntonRm <tworkn1@gmail.com>
2023-04-10 21:43:58 +0300

    second commit

--- src/bot.ts
+++ src/bot.ts
@@ -1,26 +1,34 @@
 import { Bot } from 'grammy';
-import { db } from './repo';
+import Repo from './repo';
 
-const bot = new Bot('');
+// const bot = new Bot('');
 
-bot.command('start', (ctx) => {
-	if (ctx.from) {
-		const id = ctx.from.id;
-		const name = ctx.from.first_name;
-		// save to db
-		// if exist
-		db.serialize(() => {
-			db.run('')
-		})
-	}
-})
+// bot.command('start', (ctx) => {
+// 	if (ctx.from) {
+// 		const id = ctx.from.id;
+// 		const name = ctx.from.first_name;
+// 		// save to db
+// 		// if exist
+// 		db.serialize(() => {
+// 			db.run(`CREATE TABLE IF NOT EXIST users (
+// 				id INTEGER,
+// 				name TEXT
+// 				)`)
+// 		})
+// 	}
+// })
 
-bot.on('message:text', (ctx) => {
-	ctx.reply('test')
-})
+// bot.on('message:text', (ctx) => {
+// 	ctx.reply('test')
+// })
 
 
-
+// db.serialize(() => {
+// 	db.run(`CREATE TABLE IF NOT EXISTS users (
+// 		id INTEGER,
+// 		name TEXT
+// 		)`)
+// })
 
 
 
--- src/repo.ts
+++ src/repo.ts
@@ -1,3 +1,42 @@
 import sqlite3 from 'sqlite3';
 
-export const db = new (sqlite3.verbose()).Database(':memory:');
+export default class Repo {
+	private db: sqlite3.Database
+
+	constructor() {
+		this.db = new (sqlite3.verbose()).Database('data.db', (err) => err ? console.log(err.message) : null)
+	}
+
+	createTable(name: string, params: string) {
+		this.db.serialize(() => {
+			this.db.run(`CREATE TABLE IF NOT EXISTS ${name} (${params})`)
+		})
+		this.db.close()
+	}
+	removeTable(name: string) {
+		this.db.serialize(() => {
+			this.db.run(`DROP TABLE IF EXISTS ${name}`)
+		})
+		this.db.close()
+	}
+	addValue(table: string, field: string, value: string) {
+		this.db.serialize(() => {
+			this.db.run(`INSERT INTO ${table}(${field}) VALUES (${value})`)
+		})
+		this.db.close()
+	}
+	getValue(table: string, value: string) {
+		let result: any;
+		this.db.serialize(() => {
+			this.db.get(`SELECT ${value} FROM ${table}`, (err, row) => result = row)
+		})
+		this.db.close()
+		return result
+	}
+	removeValue(table: string, value: string) {
+		this.db.serialize(() => {
+			this.db.run(`DELETE FROM ${table} WHERE ${value}`)
+		})
+		this.db.close()
+	}
+}
