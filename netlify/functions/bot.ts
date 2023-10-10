2dd81eab4f8b73dd81b2bdd0d8b19dc7058eefda
AntonRm <tworkn1@gmail.com>
2023-10-10 22:25:28 +0300

    fix fs error 2

--- netlify/functions/bot.ts
+++ netlify/functions/bot.ts
@@ -3,10 +3,10 @@ import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions"
 import OpenAI from "openai"
 import { configDotenv } from 'dotenv'
 import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url'
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+// import path from 'path'
+// import { fileURLToPath } from 'url'
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = path.dirname(__filename)
 configDotenv()
 
 const bot = new Bot(process.env.TEL_TOKEN)
@@ -34,7 +34,8 @@ bot.on('message', async (ctx) => {
 	// 	name: ctx.message.from.first_name,
 	// 	text: ctx.message.text
 	// }))
-	const file = fs.readFileSync(path.resolve(__dirname, '../', '../', 'db.json'), { encoding: 'utf-8' })
+	// const file = fs.readFileSync(path.resolve(__dirname, '../', '../', 'db.json'), { encoding: 'utf-8' })
+	const file = fs.readFileSync('../db.json', { encoding: 'utf-8' })
 	const data = JSON.parse(file)
 	console.log('data', data)
 	await ctx.reply('hello')
