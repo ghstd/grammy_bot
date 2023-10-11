4f56ced195793e7ed7e56beda25edbbe98c53fa4
AntonRm <tworkn1@gmail.com>
2023-10-11 22:28:46 +0300

    start with xata db

--- netlify/functions/bot.ts
+++ netlify/functions/bot.ts
@@ -2,72 +2,141 @@ import { Bot } from "grammy"
 import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions"
 import OpenAI from "openai"
 import { configDotenv } from 'dotenv'
-import fs from 'fs'
-// import path from 'path'
-// import { fileURLToPath } from 'url'
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = path.dirname(__filename)
+import { getXataClient } from '../../src/xata.js'
+import type { ChatCompletionMessageParam } from 'openai/resources/chat/index.js'
 configDotenv()
 
 const bot = new Bot(process.env.TEL_TOKEN)
 const openai = new OpenAI({
 	apiKey: process.env.OPENAI_API_KEY,
 })
+const xata = getXataClient()
 
-// async function getCompletion() {
-// 	const chatCompletion = await openai.chat.completions.create({
-// 		messages: [
-// 			{ role: 'user', content: 'Say this is a test' }
-// 		],
-// 		model: 'gpt-3.5-turbo'
-// 	})
-// 	console.log(chatCompletion)
-// }
+const initData: ChatCompletionMessageParam[] = [
+	{ role: 'system', content: 'you are the master of the game dungeons and dragons.' },
+	{ role: 'system', content: 'the game will take place in text dialogue format.' },
+	{ role: 'system', content: 'the game will be a short journey from the starting point to the ending point.' },
+	{ role: 'system', content: 'the journey will involve one or more players.' },
+	{ role: 'system', content: 'you can distinguish players using their IDs.' },
+	{ role: 'system', content: 'along the way, players must meet various opponents from the Dungeons and Dragons universe.' },
+	{ role: 'system', content: 'there should also be forks in the road along the way, where players will have to choose the further direction of the path.' },
+	{ role: 'system', content: 'and there should also be various kinds of obstacles on the way.' },
+	{ role: 'system', content: 'players can find various items, for example: a sword, bow, health potion, etc.' },
+	{ role: 'system', content: 'please describe the introductory scene of the beginning of the journey, and then conduct the game as the game master.' },
+	{ role: 'system', content: 'use russian language only.' },
+	{ role: 'system', content: 'translate any other language to the russian language whenever possible.' },
+	{ role: 'system', content: 'start new game' }
+]
+
+async function getCompletion(messages: ChatCompletionMessageParam[]) {
+	const chatCompletion = await openai.chat.completions.create({
+		messages: messages,
+		model: 'gpt-3.5-turbo'
+	})
+	return chatCompletion
+}
+
+async function dbGetAll() {
+	const records = await xata.db.messages
+		.select(["id", "role", "message", "userId"])
+		.getAll()
+	return records
+}
+
+async function dbDeleteAll() {
+	const records = await dbGetAll()
+	const ids = records.map((record) => record.id)
+	const deletedRecords = await xata.db.messages.delete(ids)
+	return deletedRecords
+}
+
+async function dbAddOne(data: { role: string, message: string, userId?: number }) {
+	const record = await xata.db.messages.create(data)
+	return record
+}
+
+async function getAllDialog() {
+	const records = await dbGetAll()
+	const data = records.map((record) => {
+		return {
+			role: record.role,
+			content: `${record.userId ? 'player ID: ' + record.userId + '; ' : ''}${record.message}`
+		}
+	})
+	return data as ChatCompletionMessageParam[]
+}
 
 bot.catch((error) => {
 	console.log(error)
 })
 
 bot.on('message', async (ctx) => {
-	// fs.writeFileSync(path.resolve(__dirname, '../', '../', 'db.json'), JSON.stringify({
-	// 	id: ctx.message.from.id,
-	// 	name: ctx.message.from.first_name,
-	// 	text: ctx.message.text
-	// }))
-	// const file = fs.readFileSync(path.resolve(__dirname, '../', '../', 'db.json'), { encoding: 'utf-8' })
-	const file = fs.readFileSync('../db.json', { encoding: 'utf-8' })
-	const data = JSON.parse(file)
-	console.log('data', data)
-	await ctx.reply('hello')
+	try {
+		const message = ctx.message.text.slice(17).trim()
+
+		if (message === '/start') {
+			await dbDeleteAll()
+			const completion = await getCompletion(initData)
+			const record = await dbAddOne({
+				role: 'assistant',
+				message: completion.choices[0].message.content
+			})
+			await ctx.reply(`master: ${record.message}`)
+			return
+		}
+
+		if (message === '/send') {
+			const dialog = await getAllDialog()
+			const completion = await getCompletion([...initData, ...dialog])
+			const record = await dbAddOne({
+				role: 'assistant',
+				message: completion.choices[0].message.content
+			})
+			await ctx.reply(`master: ${record.message}`)
+			return
+		}
+
+		await dbAddOne({
+			role: 'user',
+			message: message,
+			userId: ctx.from.id
+		})
+		await ctx.reply(`${ctx.from.first_name}: ${message}`)
+		return
+
+	} catch (error) {
+		console.log(error)
+		await ctx.reply('error')
+	}
 })
 
 
 
 // =========================
-// bot.start()
+bot.start()
 
-const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-	try {
-		const body = JSON.parse(event.body)
+// const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+// 	try {
+// 		const body = JSON.parse(event.body)
 
-		await bot.init()
-		await bot.handleUpdate(body)
-		return {
-			statusCode: 200,
-			body: '',
-			headers: {
-				"Access-Control-Allow-Origin": "*",
-				"Access-Control-Allow-Headers": "Content-Type",
-				"Access-Control-Allow-Methods": "GET, POST, OPTION",
-			}
-		}
-	} catch (error) {
-		console.error('error handler: ', error)
-		return { statusCode: 400, body: 'Error was here' }
-	}
-}
+// 		await bot.init()
+// 		await bot.handleUpdate(body)
+// 		return {
+// 			statusCode: 200,
+// 			body: '',
+// 			headers: {
+// 				"Access-Control-Allow-Origin": "*",
+// 				"Access-Control-Allow-Headers": "Content-Type",
+// 				"Access-Control-Allow-Methods": "GET, POST, OPTION",
+// 			}
+// 		}
+// 	} catch (error) {
+// 		console.error('error handler: ', error)
+// 		return { statusCode: 400, body: 'Error was here' }
+// 	}
+// }
 
-export { handler }
+// export { handler }
 
 
 
